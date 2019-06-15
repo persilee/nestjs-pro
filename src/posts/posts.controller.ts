5d0692a316dbf6a14543ff7c734965a6b467e484
persilee <i@lishaoy.net>
2019-06-16 07:58:09 +0800

    🍍 router of post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Query, Headers, Param } from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Post, Body } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
@@ -17,6 +17,11 @@ export class PostsController {
         return {
             title: `Post ${params.id}`
         }
+    }
+
+    @Post()
+    store(@Body() body) {
+        console.log(body)
     }
 
 }
