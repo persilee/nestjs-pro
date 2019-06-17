ddcdb0e7d1831749900ff953326d9e881f0c4715
persilee <i@lishaoy.net>
2019-06-17 09:45:38 +0800

    🥕 use middleware

--- src/core/middleware/demo.middleware.ts
+++ src/core/middleware/demo.middleware.ts
@@ -3,6 +3,8 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class DemoMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    console.log('hello ~');
+    
     next();
   }
 }
