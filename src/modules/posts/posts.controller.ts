68178bd5a418e008cfc8a9c89b99de5e2c954961
persilee <i@lishaoy.net>
2019-06-19 11:40:45 +0800

    🐵 add errorsInterceptor & use

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -6,6 +6,7 @@ import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
 import { Roles } from '../../core/decorators/roles.decorator';
 import { LoggingInterceptor } from '../../core/interceptors/logging.interceptor';
 import { TransformInterceptor } from '../../core/interceptors/transform.interceptor';
+import { ErrorsInterceptor } from '../../core/interceptors/errors.interceptor';
 
 @Controller('posts')
 // @UseFilters(DemoFilter)
@@ -16,9 +17,11 @@ export class PostsController {
     constructor(private readonly demoService: DemoService) {}
 
     @Get()
-    @UseInterceptors(TransformInterceptor)
+    // @UseInterceptors(TransformInterceptor)
+    @UseInterceptors(ErrorsInterceptor) 
     index() {
-        return this.demoService.findAll()
+        throw new ForbiddenException()
+        // return this.demoService.findAll()
     }
 
     @Get(':id')
