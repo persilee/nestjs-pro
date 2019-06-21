16682ff10504428e6b0fb96f3a624008126d8ea8
persilee <i@lishaoy.net>
2019-06-21 10:44:18 +0800

    🦉inject repository post

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Post])
+  ],
   controllers: [PostController],
   providers: [PostService]
 })
