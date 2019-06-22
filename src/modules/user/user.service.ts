701d164963dc0a2f74ca36fbc78f89a4824b5573
persilee <i@lishaoy.net>
2019-06-22 09:57:46 +0800

    🦂 add excption of user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,6 +12,13 @@ export class UserService {
   ) {}
 
   async store(data: UserDto) {
+    const { name } = data
+    const user = await this.userRepository.findOne({ name })
+
+    if (user) {
+      throw new BadRequestException('用户已存在！')
+    }
+
     const entity =  await this.userRepository.create(data)
     await this.userRepository.save(entity)
     return entity
