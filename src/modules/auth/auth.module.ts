e394f39dfecc84c91398ea86054fe874dc5ebb66
persilee <i@lishaoy.net>
2019-06-24 14:18:02 +0800

    🐟add Jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: 'B0B21D96-2723-4AB1-BF39-4980257A9A0B',
+      signOptions: {
+        expiresIn: '12h'
+      }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
