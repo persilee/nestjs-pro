a2d8178fa0d36692205e52c66c438a80626955b9
persilee <i@lishaoy.net>
2019-06-26 15:09:42 +0800

    🦍 save relation of user

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,6 +3,8 @@ import { PostService } from './post.service';
 import { async } from 'rxjs/internal/scheduler/async';
 import { PostDto } from './post.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '../../core/decorators/user.decorator';
+import { User as UserEntity } from '../user/user.entity';
 
 @Controller('posts')
 export class PostController {
@@ -12,8 +14,8 @@ export class PostController {
 
   @Post()
   @UseGuards(AuthGuard())
-  async store(@Body() data: PostDto) {
-    return await this.postService.store(data)
+  async store(@Body() data: PostDto, @User() user: UserEntity) {
+    return await this.postService.store(data, user)
   }
 
   @Get()
